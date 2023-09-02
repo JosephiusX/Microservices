c5f08bfffb549ee75f490451880d0ad4d7902f69
josephius x <josephiusx@gmail.com>
2023-09-02 03:49:09 -0700

    239. Passing Through the Cookies

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -8,15 +8,15 @@ const LandingPage = ({ currentUser }) => {
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async () => {
+LandingPage.getInitialProps = async ({ req }) => {
+  console.log(req.headers);
   if (typeof window === 'undefined') {
     //we are on the server
     // requests should be made to http://ingress-srv...
     const {data} = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
-        headers: {
-          Host: 'ticketing.dev'
-        }
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', 
+      {
+        headers: req.headers
       }
     );
 
