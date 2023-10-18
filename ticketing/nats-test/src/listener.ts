e09e39bfa057622a22a936ce3a42a869a29f3276
josephius x <josephiusx@gmail.com>
2023-10-17 22:25:29 -0700

    314. Extending the Listener

--- ticketing/nats-test/src/listener.ts
+++ ticketing/nats-test/src/listener.ts
@@ -14,29 +14,9 @@ stan.on('connect', () => {
     console.log('NATS connection closed!');
     process.exit();
   });
-  
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName('accounting-service') 
 
-  const subscription = stan.subscribe(
-    'ticket:created',
-    'queue-group-name',
-    options
-    ); // Second arg is the Queue group
-
-  subscription.on('message', (msg:Message) => {
-    const data = msg.getData();
-
-    if (typeof data === 'string') {
-      console.log(`Recieved event #${msg.getSequence()}, with data: ${data}`)
-    }
-
-    msg.ack();
-  });
-
+  new TicketCreatedListener(stan).
+  listen();
 });
 
 process.on('SIGINT', () => stan.close());
@@ -53,13 +33,13 @@ abstract class Listener {
     this.client = client;
   }
 
-  subswcriptionOptions() {
+  subscriptionOptions() {
     return this.client
       .subscriptionOptions()
       .setDeliverAllAvailable()
       .setManualAckMode(true)
       .setAckWait(this.ackWait)
-      .setDurableNane(this.queueGroupName);
+      .setDurableName(this.queueGroupName);
   }
 
   listen() {
@@ -84,5 +64,16 @@ abstract class Listener {
     return typeof data === 'string'
       ? JSON.parse(data)
       : JSON.parse(data.toString('utf8'));
+  }
+}
+
+class TicketCreatedListener extends Listener {
+  subject = 'ticket:created';
+  queueGroupName = 'payments-service';
+
+  onMessage(data: any, msg: Message) {
+    console.log('Event data!', data);
+    
+    msg.ack();
   }
 }
