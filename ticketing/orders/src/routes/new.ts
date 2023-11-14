82edf595c5a9ac8740958fc3a9b53d3afb844e5f
josephius x <josephiusx@gmail.com>
2023-11-13 18:07:00 -0800

    361. Finding Reserved Tickets

--- ticketing/orders/src/routes/new.ts
+++ ticketing/orders/src/routes/new.ts
@@ -1,7 +1,9 @@
-import mongoose from 'mongoose';9
+import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
-import {requireAuth, validateRequest } from '@jqgtickets/common';
+import {requireAuth, validateRequest, NotFoundError } from '@jqgtickets/common';
 import {body} from 'express-validator'
+import {Ticket} from '../models/ticket';
+import {Order} from '../modules/order';
 
 const router = express.Router();
 
@@ -14,6 +16,21 @@ router.get('/api/orders/', requireAuth, [
 ],
 validateRequest,
  async (req: Request, res: Response) => {
+  const {ticketId} = req.body;
+
+  const ticket = await Ticket.findById(ticketId); // Find ticket in database
+  if(!ticket) { // if not found
+    throw new NotFoundError();
+  }
+
+  // Make sure ticket is not reserved
+
+  // Calculate expiraton date for order
+
+  // Build order , save to the database
+
+  //Publish an event saying that an order was created
+
   res.send({});
 });
 
